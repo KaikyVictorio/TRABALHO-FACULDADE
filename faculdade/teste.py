6cc5c8f66f2c0c4ae14229a6503c998aa0a9a6e1
KaikyVictorio <kaikyvictorio2006@gmail.com>
2025-02-17 08:23:46 -0300

    Delete faculdade directory

--- faculdade/teste.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = 'C:/Users/Kaiky Victório/Documents/faculdade/AUDIORA/musicicon.png'
-
-# Verificar se o arquivo existe
-if os.path.exists(file_path):
-    print("O arquivo existe!")
-else:
-    print("Arquivo não encontrado!")
